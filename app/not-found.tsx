89ee6d334dafeb76b22c2ba39af836b4752acfd0
vladislavgradoblyanskyi <vladislavgradoblyanskyi@gmail.com>
2026-05-17 15:24:46 +0200

    modal ready10

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,8 @@
-import css from "./Home.module.css";
 export default function NotFound() {
   return (
     <>
-      <h1 className={css.title}>404 - Page not found</h1>
-      <p className={css.description}>
+      <h1>404 - Page not found</h1>
+      <p>
         Sorry, the page you are looking for does not exist.
       </p>
     </>
