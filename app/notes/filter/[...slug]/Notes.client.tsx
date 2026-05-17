8c2311abe02ae74820ddec1389a4a5dfa5290586
vladislavgradoblyanskyi <vladislavgradoblyanskyi@gmail.com>
2026-05-17 14:43:21 +0200

    modal ready7

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -1,7 +1,6 @@
 "use client";
 import NoteList from "@/components/NoteList/NoteList";
 import { Note } from "@/types/note";
-import css from "./Filter.module.css";
 
 type Props = {
   notes: Note[];
@@ -9,7 +8,7 @@ type Props = {
 
 export default function NotesClient({notes}: Props) {
   return (
-    <div className={css.div}>
+    <div>
       <NoteList notes={notes} />
     </div>
   );
